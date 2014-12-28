ac32718e04910ce29c49e082b03dc1bfd397391a
Ahmed Fituri <ahmed.elfituri@gmail.com>
2014-12-28 14:06:29 +0200

    adding app

--- manage.py
+++ manage.py
@@ -1,14 +1,10 @@
-"""
-WSGI config for archive project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
-"""
-
+#!/usr/bin/env python
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "archive.settings")
+import sys
 
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "archive.settings")
+
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
