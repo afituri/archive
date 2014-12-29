8387bfe23ffce3f9fc41ddaf74c209cddff7a390
anas <anas@anas-VirtualBox.(none)>
2014-12-29 03:53:46 +0200

    database settings added

--- archive/settings.py
+++ archive/settings.py
@@ -55,13 +55,28 @@ WSGI_APPLICATION = 'archive.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
-
+import json
+from pprint import pprint
+json_data=open('archive/config.json')
+data = json.load(json_data)
+json_data.close()
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': data["ENGINE"],
+        'NAME': data["NAME"],                     
+        'USER': data["USER"],
+        'PASSWORD': data["PASSWORD"],
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'db',                     
+#         'USER': 'anas',
+#         'PASSWORD': '123',
+#     }
+# }
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
