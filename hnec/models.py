3e256b87b6192f4f342825f3b0522da016b6dec2
Joabdooageel <abdooageel@github.com>
2014-12-31 15:24:21 +0200

    db

--- hnec/models.py
+++ hnec/models.py
@@ -1,3 +1,59 @@
 from django.db import models
+from django.contrib.auth.models import User 
+
 
 # Create your models here.
+class Department(models.Model):
+    name = models.CharField(max_length = 50)
+    modify_date = models.DateTimeField(auto_now=True,auto_now_add=False)
+    create_date = models.DateTimeField(auto_now=False,auto_now_add=True)
+    status = models.BoolenField(default=True)
+    def __unicode__(self):
+        return self.name
+class Section(models.Model):
+    name = models.CharField(max_length = 50)
+    modify_date = models.DateTimeField(auto_now=True,auto_now_add=False)
+    create_date = models.DateTimeField(auto_now=False,auto_now_add=True)
+    Department_id = models.ForeignKey(Department)
+    status = models.BoolenField(default=True)
+    def __unicode__(self):
+        return self.name
+
+class Archive(models.Model):
+    name = models.CharField(max_length = 50)
+    modify_date = models.DateTimeField(auto_now=True,auto_now_add=False)
+    create_date = models.DateTimeField(auto_now=False,auto_now_add=True)
+    real_date = models.DateTimeField()
+    section_id = models.ForeignKey(Section)
+    department_id = models.ForeignKey(Department)
+    ref_num = models.CharField(max_length = 50)
+    text = models.CharField(max_length = 500)
+    status = models.BoolenField(default=True)
+    def __unicode__(self):
+        return self.name
+
+class Files(models.Model):
+    path = models.URLField()
+    create_date = models.DateTimeField(auto_now=False,auto_now_add=True)
+    archive_id =  models.ForeignKey(Archive)
+    status = models.BoolenField(default=True)
+    def __unicode__(self):
+        return self.path
+
+class Employee(models.Model):
+    user = models.OneToOneField(User)
+    department_id = models.ForeignKey(Department)
+    def __unicode__(self):
+        return self.department_id
+
+class Log(models.Model):
+    id_user = models.ForeignKey(User)
+    action_type = models.CharField(max_length = 50)
+    tabel = models.CharField(max_length = 50)
+    desc = models.CharField(max_length = 50)
+    create_date = models.DateTimeField(auto_now=False,auto_now_add=True)
+    tabel_id = models.IntegerField()
+    value = models.CharField(max_length = 50)
+    def __unicode__(self):
+        return self.action_type
+
