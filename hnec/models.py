8b7128e448c461d2ffbde6f34b11eba80491856e
abdooageel <ageel.abdoo@gmail.com>
2015-01-01 14:47:47 +0200

    model

--- hnec/models.py
+++ hnec/models.py
@@ -1,23 +1,33 @@
 from django.db import models
 from django.contrib.auth.models import User 
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+ 
 
 
-# Create your models here.
+def upload_file(instance,name):
+    return os.path.join(os.path.dirname(BASE_DIR),"learn_django", "static")+"/up/"+name
+
 class Department(models.Model):
     name = models.CharField(max_length = 50)
     modify_date = models.DateTimeField(auto_now=True,auto_now_add=False)
     create_date = models.DateTimeField(auto_now=False,auto_now_add=True)
     status = models.BoolenField(default=True)
+
     def __unicode__(self):
         return self.name
+
+
 class Section(models.Model):
     name = models.CharField(max_length = 50)
     modify_date = models.DateTimeField(auto_now=True,auto_now_add=False)
     create_date = models.DateTimeField(auto_now=False,auto_now_add=True)
     Department_id = models.ForeignKey(Department)
     status = models.BoolenField(default=True)
+
     def __unicode__(self):
         return self.name
+
 
 class Archive(models.Model):
     name = models.CharField(max_length = 50)
@@ -29,23 +39,29 @@ class Archive(models.Model):
     ref_num = models.CharField(max_length = 50)
     text = models.CharField(max_length = 500)
     status = models.BoolenField(default=True)
+
     def __unicode__(self):
         return self.name
 
+
 class Files(models.Model):
     name = models.CharField(max_length = 50)
-    path = models.URLField()
+    path = models.FileField(upload_to=upload_file)
     create_date = models.DateTimeField(auto_now=False,auto_now_add=True)
     archive_id =  models.ForeignKey(Archive)
     status = models.BoolenField(default=True)
+
     def __unicode__(self):
         return self.name
+
 
 class Employee(models.Model):
     user = models.OneToOneField(User)
     department_id = models.ForeignKey(Department)
+
     def __unicode__(self):
         return self.department_id
+
 
 class Log(models.Model):
     id_user = models.ForeignKey(User)
@@ -55,6 +71,7 @@ class Log(models.Model):
     create_date = models.DateTimeField(auto_now=False,auto_now_add=True)
     tabel_id = models.IntegerField()
     value = models.CharField(max_length = 50)
+
     def __unicode__(self):
         return self.action_type
 
