f7925dbc4f5b6455f87383e1a87ad7275ee9e236
anasbhih <bhih.anas@gmail.com>
2015-01-06 12:50:28 +0200

    making direct

--- archive/urls.py
+++ archive/urls.py
@@ -3,11 +3,12 @@ from django.contrib import admin
 from hnec.views import *
 
 urlpatterns = patterns('',
-
+	url(r'^admin/', include(admin.site.urls)),
 	url(r'^$', home),
     url(r'^department/$', department),
     url(r'^addArchive/$', addArchive),
     url(r'^editArchive/$', editArchive),
     url(r'^addFolder/$', addFolder),
-
+    url(r'^auth/$', auth_view),
+    url(r'^department/(?P<department_id>\d+)/$',department),
 )
--- hnec/admin.py
+++ hnec/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+# Register your models here.
+from .models import Employee, Department
+
+class EmployeeAdmin(admin.ModelAdmin):
+	list_display=('user','department_id')
+	class Meta:
+		model = Employee
+
+class DepartmentAdmin(admin.ModelAdmin):
+	list_display=('name',)
+	class Meta:
+		model = Department
+
+admin.site.register(Department, DepartmentAdmin)
+admin.site.register(Employee, EmployeeAdmin)
