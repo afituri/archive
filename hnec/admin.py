4bb5e5147fdf13001c630edd8bd46488db68b014
Your Name <you@example.com>
2015-01-08 15:16:51 +0200

    edit

--- hnec/admin.py
+++ hnec/admin.py
@@ -6,6 +6,11 @@ from django.contrib import admin
 # Register your models here.
 from .models import *
 
+class FilesAdmin(admin.ModelAdmin):
+	list_display=('name',)
+	class Meta:
+		model = Files
+
 class EmployeeAdmin(admin.ModelAdmin):
 	list_display=('user','department_id')
 	class Meta:
@@ -30,3 +35,4 @@ admin.site.register(Archive, ArchiveAdmin)
 admin.site.register(Department, DepartmentAdmin)
 admin.site.register(Section, SectionAdmin)
 admin.site.register(Employee, EmployeeAdmin)
+admin.site.register(Files, FilesAdmin)
