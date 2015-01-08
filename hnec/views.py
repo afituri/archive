5172a54d5a35f9628ae0cc2010b50d912823c796
anasbhih <bhih.anas@gmail.com>
2015-01-08 09:37:44 +0200

    views spaces

--- hnec/views.py
+++ hnec/views.py
@@ -12,13 +12,15 @@ from .models import *
 from django.contrib import messages
 
 # Create your views here.
-
+@staff_member_required
 def cpanel(request):
     if request.user.is_staff:
         print Department.objects.all()
         return render_to_response('cpanel.html',{'department':Department.objects.all()})
     else:
         return HttpResponseRedirect('/')
+
+
 
 def addFolder(request, department_id=1):
     c = {}
@@ -27,25 +29,33 @@ def addFolder(request, department_id=1):
         {'sections':Section.objects.filter(Department_id=department_id)},)
         
 
+
 def addDepartment(request):
     c = {}
     c.update(csrf(request))
     return render_to_response('addDepartment.html',c)
+
+
 
 def logIn(request):
     c = {}
     c.update(csrf(request))
     return render_to_response('logIn.html',c)
 
+
+
 def editArchive(request):
     c = {}
     c.update(csrf(request))
     return render_to_response('editArchive.html',c)
 
+
+
 def addArchive(request):
     c = {}
     c.update(csrf(request))
     return render_to_response('addArchive.html',c)
+
 
 
 @login_required(login_url='/')
@@ -76,11 +86,12 @@ def auth_view(request):
         messages.warning(request, 'إسم المستخدم أو كلمة المرور غير صحيحة')
         return render_to_response('logIn.html', locals(), 
         context_instance=RequestContext(request))
+
+
         
 def sign(request):
     c = {}
     c.update(csrf(request))
     return render_to_response('sign.html',c)
 
-#@staff_member_required for cpanel
 
