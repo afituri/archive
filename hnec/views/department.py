8b9c4d5771f73196d3292da49a740aef309ecec0
abdelqudoss <doss.aburass@gmail.com>
2015-01-12 15:26:15 +0200

    department

--- hnec/views/department.py
+++ hnec/views/department.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response,redirect
 from django.core.context_processors import csrf
 from django.contrib import auth
 from django.http import HttpResponseRedirect
@@ -9,6 +9,8 @@ from django.contrib.auth.decorators import permission_required
 from django.contrib.auth import authenticate
 from hnec.models import *
 from django.db.models import Count
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 
 
 @login_required(login_url='/')
@@ -20,10 +22,38 @@ def addFolder(request, department_id=1):
 
 
 @login_required(login_url='/')
+def Departments(request):
+        c = {}
+        c.update(csrf(request))
+        objects=Department.objects.filter(status=True)
+        paginator=Paginator(objects,2)
+        page = request.GET.get('page')
+        try:
+            dept = paginator.page(page)
+        except PageNotAnInteger :
+            dept = paginator.page(1)
+        except EmptyPage:
+            dept = paginator.page(paginator.num_pages)
+        # except paginator.page_range
+        c['department']=dept
+        return render_to_response('Departments.html',c)
+
+@login_required(login_url='/')   
+def addDept(request):
+    name = request.POST.get('Deptname','')
+    dep = Department(name = name, status = True)
+    dep.save()
+    return  redirect('../Departments/')
+
+
+
+@login_required(login_url='/')
 def addDepartment(request):
     c = {}
     c.update(csrf(request))
-    return render_to_response('addDepartment.html',c)
+    return render_to_response('addFolder.html',
+        {'sections':Section.objects.filter(Department_id=department_id)},)
+
 
 
 @login_required(login_url='/')
@@ -37,6 +67,8 @@ def department(request, department_id=1):
                                     },    )
     else:
         return HttpResponseRedirect('/department/%s/' %request.user.employee.department_id.id)
+
+
 
 @login_required(login_url='/')
 def addDepartment(request):
